3209e0e6d9af1bdbf9c3b52cac669977f02a3e70
julen <julen817@gmail.com>
2025-02-24 17:46:03 +0100

    created test base

---
+++ src/test/java/es/dsrroma/school/spring/boot/reuniones/ReunionesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dsrroma.school.spring.boot.reuniones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReunionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
