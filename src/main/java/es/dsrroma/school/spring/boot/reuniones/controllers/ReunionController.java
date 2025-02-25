e46f9cbb94e77c90a04bab3c0ea6e2962d9c3683
julen <julen817@gmail.com>
2025-02-25 14:32:25 +0100

    Added test MVC for 'reuniones'

---
+++ src/main/java/es/dsrroma/school/spring/boot/reuniones/controllers/ReunionController.java
@@ -0,0 +1,30 @@
+package es.dsrroma.school.spring.boot.reuniones.controllers;
+
+import es.dsrroma.school.spring.boot.reuniones.models.Reunion;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/reuniones")
+public class ReunionController {
+
+    private static final List<Reunion> reuniones = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 5; i++) {
+            reuniones.add(new Reunion(i, "Reunión " + i, ZonedDateTime.now().plusDays(i)));
+        }
+    }
+
+    @GetMapping
+    public String getAllReuniones(Model model){
+        model.addAttribute("reuniones", reuniones);
+        return "reuniones";
+    }
+}
